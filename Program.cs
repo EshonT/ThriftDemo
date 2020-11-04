104150ca561f8bbb32972a9d2936754bc2430125
TiX <tyx.cell@gmail.com>
2020-11-04 10:47:42 +0800

    Init C# Project

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using CSharpDemo.Thrift;
+using CSharpDemo.Thrift.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Thrift;
+using Thrift.Protocol;
+using Thrift.Server;
+using Thrift.Transport;
+
+namespace CSharpDemo
+{
+    class Program
+    {
+        private static TServer server;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("启动服务");
+            try
+            {
+                TServerSocket serverSocket = new TServerSocket(58622);
+                TBinaryProtocol.Factory factory = new TBinaryProtocol.Factory();
+
+                TProcessor processor = new ThriftService.Processor(new ThriftServiceImpl());
+                server = new TSimpleServer(processor, serverSocket);
+                server.Serve();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                return;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Thrift/Impl/ThriftServiceImpl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpDemo.Thrift.Impl
+{
+    internal class ThriftServiceImpl : ThriftService.Iface
+    {
+        public void Connect(Dictionary<string, string> machine)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Dictionary<string, TestClass> GetMap()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Dictionary<string, int> GetValue()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
